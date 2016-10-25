c38976725fdb5247f434deba190c2073e93aae99
YaFeng-Xiong <yafeng.xiong110@gmail.com>
2016-10-25 13:07:58 -0600

    nothing

--- app/src/main/java/com/yafengxiong/fblamobileapp/MainActivity.java
+++ app/src/main/java/com/yafengxiong/fblamobileapp/MainActivity.java
@@ -45,17 +45,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
-    @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
