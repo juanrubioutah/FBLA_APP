058990a7c71325528b3f0e129552be8fe0a56911
YaFeng-Xiong <yafeng.xiong110@gmail.com>
2016-12-01 13:17:31 -0700

    Revert "Juan's work"
    This reverts commit b7eb6abb06ea9b14bfbe7fe9297f4d2ed12e1143.

--- app/src/main/java/com/yafengxiong/fblamobileapp/MainActivity.java
+++ app/src/main/java/com/yafengxiong/fblamobileapp/MainActivity.java
@@ -53,11 +53,10 @@ public class MainActivity extends ActionBarActivity {
 
     private ArrayList<ImageItem> getData(){
         final ArrayList<ImageItem> imageItems = new ArrayList<>();
-        //TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
-        int[] imgs = new int[10];
-        for(int i = 0; i < imgs.length; i++){
-           // Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
-            //imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for(int i = 0; i < imgs.length(); i++){
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+            imageItems.add(new ImageItem(bitmap, "Image#" + i));
         }
         return imageItems;
     }
